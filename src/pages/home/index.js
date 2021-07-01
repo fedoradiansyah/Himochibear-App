7b02a9ccd93a605985b4f22c575ddc00eaf6737d
Ferdiansyah Permana Ninditha <fedoradiansyah@gmail.com>
2021-07-01 11:52:39 +0700

    Init Project

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class home extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
